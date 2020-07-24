f51e1fbe512b30231be71e9ad3156eca35b64bf1
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-25 00:51:17 +0600

    Add files via upload

---
+++ MathObject.js
@@ -0,0 +1,16 @@
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+let num=9.6;
+console.log(Math.round(num));
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
+console.log(Math.trunc(num)); //Only integer part
+let num2=144;
+console.log(Math.sqrt(num2));
+
+let randomNumber=Math.random();
+console.log(randomNumber);
+console.log(Math.round((randomNumber*10)))
