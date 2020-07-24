eda7fcfe49a990e56f2131d54a876bb511ab239b
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-25 00:47:00 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+console.time("Code took time");
+console.log("Hello World");
+console.log(100+300);
+console.log([12,56,87,"Zakir","Want a job"]);
+console.table({name:"Zakir", age:25, height:6.7});
+console.warn("This is warning");
+console.assert(68>345,"This is not possible and Assertion failed");
+console.error("Error Occured");
+console.timeEnd("Code took time");
+// console.clear();
