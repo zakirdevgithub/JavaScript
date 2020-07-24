31c97b354eeb1770213add39c7f532b25df03b83
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-25 00:50:29 +0600

    Add files via upload

---
+++ WindowsObject.js
@@ -0,0 +1,24 @@
+// window.alert("This is alert");
+//we can neglect window keyword because it is global
+
+// alert("This is alert");
+// let variable= window.prompt("Enter your name");
+// console.log(variable);
+
+// let confirmation= window.confirm("it is confirm");
+//if anyone click 'ok' it return true else if click 
+// 'cancel' it return cancel
+// console.log(confirmation);
+
+let height= innerHeight;
+console.log(height);
+let width= innerWidth;
+console.log(width);
+
+let scrlx= scrollX;
+console.log(scrlx);
+let scrly= scrollY;
+console.log(scrly);
+
+let hist= history;
+console.log(hist);
