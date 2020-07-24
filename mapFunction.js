e49addc8e8c49b2ea653d0cf6761e637aeb7930d
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-25 00:52:53 +0600

    Add files via upload

---
+++ mapFunction.js
@@ -0,0 +1,8 @@
+let myArray=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,19,37,56,89];
+
+//map function return a Array with true, false
+let oddNumbers=myArray.map((e)=>{
+    return e%2==1;
+})
+console.log(oddNumbers);
+
